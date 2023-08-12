4ce67f0f50c5c1e8fe7312b415e91b916bcdd148
Vansh <vanshpthakkar@gmail.com>
2023-08-12 14:26:13 +0530

    commit#7

--- pss/settings.py
+++ pss/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bqdl(+)=$71fh54*9qvs8*499w2%v%4k64fl2zmr_yl222jx+&"
-
+secret_key = get_random_secret_key()
+SECRET_KEY = secret_key
+# "django-insecure-bqdl(+)=$71fh54*9qvs8*499w2%v%4k64fl2zmr_yl222jx+&"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -118,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT=os.path.join(BASE_DIR,'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
